80b44c818078624306e28c1d19b7ed69a171bae9
Diego Dorado <diegodorado@gmail.com>
2020-03-29 14:58:09 -0300

    adds a subproject

---
+++ malvinas/src/index.js
@@ -0,0 +1,13 @@
+// Test import of a JavaScript function, an SVG, and Sass
+import HelloWorld from './js/HelloWorld'
+import WebpackLogo from './images/webpack-logo.svg'
+import './styles/index.sass'
+
+
+// Create heading node
+const greeting = document.createElement('h1')
+greeting.textContent = HelloWorld()
+
+// Append SVG and heading nodes to the DOM
+const app = document.querySelector('#root')
+app.append( greeting)
