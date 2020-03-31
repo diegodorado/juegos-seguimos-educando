bf38963e0b5ce1e4f0608311f9707366168ebcd0
Diego Dorado <diegodorado@gmail.com>
2020-03-31 11:17:52 -0300

    makes initial message a modal message

--- malvinas/src/index.js
+++ malvinas/src/index.js
@@ -4,29 +4,138 @@ import {Howl} from 'howler'
 
 const SIZE = 2
 
+const placements = new Array(SIZE*SIZE).fill(-1)
+let zIndex = 300
+let playing = false
+
 //creates all dom elements dinamically
 const app = document.querySelector('#root')
 const draggables = document.createElement('div')
 const droppables = document.createElement('div')
-droppables.className = 'puzzle'
-const title = document.createElement('h1')
-title.innerText = 'ARMAR EL ROMPECABEZAS COLOCANDO LAS PIEZAS DONDE CORRESPONDEN.'
-
-const winBox = document.createElement('div')
-winBox.className = 'win-box'
-winBox.style = 'display:none'
-const winTitle = document.createElement('h2')
-winTitle.innerText = '¡Muy bien! Completaste el mapa'
-const replayBtn = document.createElement('button')
-replayBtn.innerText = 'Jugar de nuevo'
-replayBtn.addEventListener('click', () => window.location.reload())
-winBox.append(winTitle)
-winBox.append(replayBtn)
-
-app.append(title)
 app.append(droppables)
 app.append(draggables)
-app.append(winBox)
+
+const showModal = (text, btnText, btnAction) => {
+  const modelBox = document.createElement('div')
+  modelBox.className = 'win-box'
+  const title = document.createElement('h2')
+  title.innerText = text
+  const btn = document.createElement('button')
+  btn.innerText = btnText
+  btn.addEventListener('click', () => {
+    btnAction()
+    app.removeChild(modelBox)
+  })
+  modelBox.append(title)
+  modelBox.append(btn)
+  app.append(modelBox)
+}
+
+const createBoard = () => {
+
+  droppables.style.gridTemplateColumns = `repeat(${SIZE}, 1fr)`
+  droppables.className = 'puzzle'
+
+  for (let i=0;i<SIZE;i++){
+    for (let j=0;j<SIZE;j++){
+      const tile = document.createElement('div')
+      tile.className = 'draggable tile'
+      tile.style.backgroundSize = `${SIZE*100}%  ${SIZE*100}% `
+      tile.style.backgroundPositionX = `${-j*100}%`
+      tile.style.backgroundPositionY = `${-i*100}%`
+      draggables.append(tile)
+
+      const place = tile.cloneNode()
+      place.className = 'dropzone tile'
+      droppables.append(place)
+
+      // remember tile index
+      place.index = tile.index = (i*SIZE+j)
+    }
+  }
+
+  // enable draggables to be dropped into this
+  interact('.dropzone').dropzone({
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.25,
+
+    ondropactivate: function (ev) {
+      ev.target.classList.add('drop-active')
+    },
+
+    ondragenter: function (ev) {
+      var draggableElement = ev.relatedTarget
+      var dropzoneElement = ev.target
+      dropzoneElement.classList.add('drop-target')
+      draggableElement.classList.add('can-drop')
+      play('dragEnter')
+    },
+
+    ondragleave: function (ev) {
+      ev.target.classList.remove('drop-target')
+      ev.relatedTarget.classList.remove('can-drop')
+    },
+
+    ondrop: function (ev) {
+      placeOver(ev.target,ev.relatedTarget)
+      ev.relatedTarget.classList.remove('can-drop')
+      placements[ev.target.index] = ev.relatedTarget.index
+      play('drop')
+      checkForWin()
+    },
+    ondropdeactivate: function (ev) {
+      ev.target.classList.remove('drop-active')
+      ev.target.classList.remove('drop-target')
+    },
+
+    checker: (dragEvent,         // related dragmove or dragend
+                       event,             // Touch, Pointer or Mouse Event
+                       dropped,           // bool default checker result
+                       dropzone,          // dropzone Interactable
+                       dropElement,       // dropzone element
+                       draggable,         // draggable Interactable
+                       draggableElement) => {// draggable element
+
+      // return dropped && (placements[dropElement.index] === -1)
+      return dropped && (draggableElement.index===dropElement.index)
+    }
+
+  })
+
+  interact('.draggable').draggable({
+    inertia: true,
+    listeners: { 
+      start: (ev) =>{
+        if(!playing)
+          ev.interaction.stop()
+        else{
+          ev.target.style.zIndex = `${zIndex++}`
+          placements.forEach( (x,i) => {
+            if(x===ev.target.index)
+              placements[i] = -1
+          })
+          play('drag')
+        }
+
+      },
+      move: (ev) =>{
+        ev.target.style.top = `${ev.client.y-ev.target.offsetHeight/2}px`
+        ev.target.style.left = `${ev.client.x-ev.target.offsetWidth/2}px`
+      },
+      end: (ev) =>{
+        ev.target.style.zIndex = '99'
+      }
+    }
+  })
+
+}
+
+
+const startGame = () => {
+  placements.forEach( (x,i) => placements[i] = -1)
+  resized()
+  playing=true
+}
 
 //sounds
 const sounds = {
@@ -47,104 +156,12 @@ const play = (s) => {
 }
 
 
-const placements = new Array(SIZE*SIZE).fill(-1)
-
-droppables.style.gridTemplateColumns = `repeat(${SIZE}, 1fr)`
-
-for (let i=0;i<SIZE;i++){
-  for (let j=0;j<SIZE;j++){
-    const tile = document.createElement('div')
-    tile.className = 'draggable tile'
-    tile.style.backgroundSize = `${SIZE*100}%  ${SIZE*100}% `
-    tile.style.backgroundPositionX = `${-j*100}%`
-    tile.style.backgroundPositionY = `${-i*100}%`
-    draggables.append(tile)
-
-    const place = tile.cloneNode()
-    place.className = 'dropzone tile'
-    droppables.append(place)
-
-    // remember tile index
-    place.index = tile.index = (i*SIZE+j)
-  }
-}
-
-
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.25,
-
-  ondropactivate: function (ev) {
-    ev.target.classList.add('drop-active')
-  },
-
-  ondragenter: function (ev) {
-    var draggableElement = ev.relatedTarget
-    var dropzoneElement = ev.target
-    dropzoneElement.classList.add('drop-target')
-    draggableElement.classList.add('can-drop')
-    play('dragEnter')
-  },
-
-  ondragleave: function (ev) {
-    ev.target.classList.remove('drop-target')
-    ev.relatedTarget.classList.remove('can-drop')
-  },
-
-  ondrop: function (ev) {
-    placeOver(ev.target,ev.relatedTarget)
-    ev.relatedTarget.classList.remove('can-drop')
-    placements[ev.target.index] = ev.relatedTarget.index
-    play('drop')
-    checkForWin()
-  },
-
-  ondropdeactivate: function (ev) {
-    ev.target.classList.remove('drop-active')
-    ev.target.classList.remove('drop-target')
-  },
-
-  checker: (dragEvent,         // related dragmove or dragend
-                     event,             // Touch, Pointer or Mouse Event
-                     dropped,           // bool default checker result
-                     dropzone,          // dropzone Interactable
-                     dropElement,       // dropzone element
-                     draggable,         // draggable Interactable
-                     draggableElement) => {// draggable element
-
-    // return dropped && (placements[dropElement.index] === -1)
-    return dropped && (draggableElement.index===dropElement.index)
-  }
-
-})
-
-let zIndex = 300
-
-interact('.draggable').draggable({
-  inertia: true,
-  listeners: { 
-    start: (ev) =>{
-      ev.target.style.zIndex = `${zIndex++}`
-      winBox.style = 'display:none'
-      placements.forEach( (x,i) => {
-        if(x===ev.target.index)
-          placements[i] = -1
-      })
-      play('drag')
-    },
-    move: (ev) =>{
-      ev.target.style.top = `${ev.client.y-ev.target.offsetHeight/2}px`
-      ev.target.style.left = `${ev.client.x-ev.target.offsetWidth/2}px`
-    }
-  }
-})
 
 const getOffset = (el) => {
-    var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  var rect = el.getBoundingClientRect(),
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
 
 const checkForWin = () => {
@@ -152,7 +169,8 @@ const checkForWin = () => {
   const won = placements.map((x,i) => x===i).every(x => x)
   if(won){
     setTimeout(() => {
-      winBox.style = ''
+      showModal('¡Muy bien! Completaste el mapa','Jugar de nuevo',startGame)
+      playing=false
       play('win')
     },250)
   }
@@ -202,8 +220,7 @@ const placeOver = (src, tgt) =>{
 }
 
 window.addEventListener('resize', debounce(resized,10, false), false)
-
 window.addEventListener('orientationchange',resized, false)
 
-resized()
-
+createBoard()
+showModal('Armar el rompecabezas colocando las piezas donde corresponden.','Empezar', startGame)
